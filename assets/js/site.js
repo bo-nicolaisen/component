4978d19803ed40f2887b3d3fb8137c959d9edcb4
bo-nicolaisen <bni@techcollege.dk>
2024-08-26 09:21:17 +0200

    done

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,19 @@
 // write cool JS hwere!!
+
+
+let myIcon = document.getElementById('icon')
+let myDropDown = document.getElementById('dropdownContent')
+
+
+
+myIcon.addEventListener('click', (event) => {
+
+    myDropDown.classList.toggle('hidden')
+
+})
+
+function callback(myInfo) {
+    console.log(myInfo);
+    myDropDown.classList.toggle('hidden')
+
+}
